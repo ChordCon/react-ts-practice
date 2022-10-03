e706dc783da77ba4087d577c82a51094640cdfc7
HyunSoo Park <gustn400@naver.com>
2022-10-03 20:17:47 +0900

    22.10.03 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value);
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="user name"
-        ></input>
-        <button>Log in</button>
-      </form>
-      <div>hello {value}</div>
-    </div>
+    <Container>
+      <H1>hi</H1>
+    </Container>
   );
 }
 
 export default App;
-/*
-(e: React.FormEvent<HTMLInputElement>)
-타입스트립트에게 e가 어떤 것인지 설명
-위에서는 form내부의
-HTMLInputElement의 이벤트라는 것을 설명
-
-
-e.preventDefault()
-form에서는 엔터를 누르면 리프레시가 되는데 이를 방지해준다.
-*/
